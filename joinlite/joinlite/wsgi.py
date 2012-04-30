e6c044f53ab77c5fea689f3dcc6a2b96fdc9091b
Kwan Hong Lee <kwan@media.mit.edu>
2012-04-30 00:32:40 -0400

    works on the server now

--- joinlite/joinlite/wsgi.py
+++ joinlite/joinlite/wsgi.py
@@ -14,7 +14,9 @@ framework.
 
 """
 import os
+import site
 
+site.addsitedir('/home/www/tplusone.net/joinlite')
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "joinlite.settings")
 
 # This application object is used by any WSGI server configured to use this
